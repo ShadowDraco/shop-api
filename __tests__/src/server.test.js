992174f202c0932a8479d62b2c1289997fe2c3cc
Ethan Storm <stormethan10@gmail.com>
2023-05-25 14:19:02 -0700

    adds first test

---
+++ __tests__/src/server.test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+process.env.SECRET = "TEST_SECRET";
+process.env.NODE_ENV = "test";
+
+const supertest = require("supertest");
+
+const { server } = require("../../src/server.js");
+const { db } = require("../../src/models/index");
+
+beforeAll(async () => {
+  await db.sync();
+});
+
+afterAll(async () => {
+  await db.drop();
+});
+
+let userData = {
+  testUser: { username: "user", password: "password" },
+  testAdmin: { username: "admin", password: "admin", role: "admin" },
+};
+
+let badAccessToken = null;
+let goodAccessToken = null;
+
+const request = supertest(server);
+
+describe("Server", () => {
+  it("runs", async () => {
+    const response = await request.get("/");
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual("Home route!");
+  });
+});
